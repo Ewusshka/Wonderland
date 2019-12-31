1e4bc8dd3ac7afac2cac541972da36c5b924ecb8
EfkaEvinka <eva.vaculikova@nkp.cz>
2019-12-31 19:23:18 +0100

    Update .js translate functions do not work

---
+++ JS_script1.js
@@ -0,0 +1,27 @@
+function myFunction1() {
+    document.getElementById("para1").innerHTML = 
+        "775 61 53 54<br>
+        <a href="mailto:vaculikova.eva@yahoo.com">vaculikova.eva@yahoo.com</a><br>
+        Šantrochova 14, 162 00 Praha<br>
+        <h2 class="heading">Jazyky:</h2>
+        <ul>
+            <li>Slovensky: C2</li>
+            <li>English: C1</li>
+            <li>Deutsch: B2 - C1</li>
+            <li>Pусский: B2</li>
+        </ul>
+        <h2 class="heading">Schopnosti:</h2>
+        <ul>
+            <li>Bash: začátečník </li>
+            <li>certifikát v pojišťovnictví</li>
+            <li>certifikát MS Excel a Word</li>
+        </ul>
+        <h2 class="heading">Zájmy:</h2>
+        <ul>
+            <li>filmy: psychologické, dokumenty, politické</li> 
+            <li>knihy</li>
+            <li>divadlo</li>
+            <li>cestování</li>
+            <li>běh, Krav Maga, yoga</li>
+        </ul>";
+}
---
+++ JS_script2.js
@@ -0,0 +1,27 @@
+function myFunction2() {
+    document.getElementById("para1").innerHTML = 
+        "775 61 53 54<br>
+        <a href="mailto:vaculikova.eva@yahoo.com">vaculikova.eva@yahoo.com</a><br>
+        Šantrochova 14, 162 00 Praha<br>
+        <h2 class="heading">Jazyky:</h2>
+        <ul>
+            <li>Slovensky: C2</li>
+            <li>English: C1</li>
+            <li>Deutsch: B2 - C1</li>
+            <li>Pусский: B2</li>
+        </ul>
+        <h2 class="heading">Schopnosti:</h2>
+        <ul>
+            <li>Bash: začiatočník </li>
+            <li>certifikát v poisťovníctve</li>
+            <li>certifikát MS Excel a Word</li>
+        </ul>
+        <h2 class="heading">Záujmy:</h2>
+        <ul>
+            <li>filmy: psychologické, dokumenty, politické</li> 
+            <li>knihy</li>
+            <li>divadlo</li>
+            <li>cestovanie</li>
+            <li>beh, Krav Maga, yoga</li>
+        </ul>";
+}
