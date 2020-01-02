4cba44f79dd1fd361418549294588114aaa5d6dd
Pedro <pholik1@gmail.com>
2020-01-02 13:47:10 +0100

    js3 example, translate

---
+++ JS_script3.js
@@ -0,0 +1,10 @@
+function myFunction3() {
+  alert("funkcni javascript");
+}
+
+function translateCzechSlovak() {
+  document.getElementById("bash_level").innerHTML =
+    document.getElementById("bash_level").innerHTML === "začiatočník"
+      ? "začátečník"
+      : "začiatočník";
+}
