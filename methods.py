eca2b8172403095086777ca7acebb9fee8e5ae69
EfkaEvinka <eva.vaculikova@nkp.cz>
2020-02-12 17:28:37 +0100

    Update 12.2.2020

--- methods.py
+++ methods.py
@@ -3,16 +3,34 @@ def add(a, b):
     print("Výsledok je: " + str(c))
     return c
 
-def add_eva(a, b):
-    if (a > 0 and b > 0):
-        c = a + b
-        return c
-    else:
-        return "Nejsou kladne cisla"
-    
 
-if __name__ == "__main__":
-    print('tady je taky kod')
-    print(__name__)
-    result = add_eva(-1, 2)
-    print(result)
+def sub(a, b):
+    c = a - b
+    print("Výsledok je: " + c)
+    return c
+
+
+def mul(a, b):
+    c = a * b
+    print("Výsledok je: " + c)
+    return c
+
+
+def div(a, b):
+    c = a / b
+    print("Výsledok je: " + c)
+    return c
+
+# def add_eva(a, b):
+#   if (a > 0 and b > 0):
+#       c = a + b
+#       return c
+#   else:
+#       return "Nejsou kladne cisla"
+
+
+# if __name__ == "__main__":
+#   print('tady je taky kod')
+#   print(__name__)
+#	result = add_eva(-1, 2)
+# 	print(result)
